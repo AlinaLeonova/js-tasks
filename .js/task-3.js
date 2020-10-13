b33f87a76d9dddcbdc8247624d1276d3c449d2c9
Alina Leonova <alinaa.leonova@gmail.com>
2020-10-13 12:10:45 +0300

    task 3

--- .js/task-3.js
+++ .js/task-3.js
@@ -1,27 +1,27 @@
 class Storage {
-    constructor(items) {
-        this.items = items
-    }
-    getItems() {
-        return this.items
-    }
-    addItem(item) {
-        return this.items.push(item)
-    }
-    removeItem(item) {
+  constructor(items) {
+    this.items = items
+  }
+  getItems() {
+    return this.items
+  }
+  addItem(item) {
+    return this.items.push(item)
+  }
+  removeItem(item) {
 
 
-        //! for (let i = 0; i < goods.length; i++) {
-        //?     if (i >= 0)
-        //?         return this.items.splice(1, 2)
-        //? }
+    //! for (let i = 0; i < goods.length; i++) {
+    //?     if (i >= 0)
+    //?         return this.items.splice(1, 2)
+    //? }
 
 
-        // let index = this.items.indexOf(item);
-        // if (index >= 0) {
-        //     return this.items.splice(index, 1);
-        // }
+    let index = this.items.indexOf(item);
+    if (index >= 0) {
+      return this.items.splice(index, 1);
     }
+  }
 
 }
 
@@ -30,10 +30,10 @@ console.log(typeof Storage);
 // 'function'
 
 const goods = [
-    'Нанитоиды',
-    'Пролонгер',
-    'Железные жупи',
-    'Антигравитатор'
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
 ];
 
 const storage = new Storage(goods);
